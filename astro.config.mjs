029c48034f60e4ce22e94a91e0cbe0e2a8e7ae71
Eddy Varela <eddvar@amazon.com>
2024-01-15 20:09:16 -0800

    first-commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "astro/config";
+import awsAmplify from "astro-aws-amplify";
+
+export default defineConfig({
+  output: "server", // output: 'hybrid'
+  adapter: awsAmplify(),
+});
