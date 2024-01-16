e659de2be3f2dbfa449bb01859721c820f544059
Eddy Varela <eddvar@amazon.com>
2024-01-15 20:25:51 -0800

    add amplify-hosting dir

---
+++ .amplify-hosting/compute/default/chunks/generic_jbS_EuZx.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/generic_8btK1Q1-.mjs');
+
+export { page };
